20038379eb4e523af7954b111c40a07426c40c1b
Nick Clear <nickclear22@gmail.com>
2019-01-30 22:18:03 -0800

    Updates

---
+++ precache-manifest.bd68998ee85d6555b1e9085877e83ba2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "779673676c61f705a24b",
+    "url": "/static/js/main.77967367.chunk.js"
+  },
+  {
+    "revision": "e4b1d9321cd380659916",
+    "url": "/static/js/1.e4b1d932.chunk.js"
+  },
+  {
+    "revision": "779673676c61f705a24b",
+    "url": "/static/css/main.32ec6a49.chunk.css"
+  },
+  {
+    "revision": "3fc8f61bc65b560019c4339b156c37dc",
+    "url": "/index.html"
+  }
+];
